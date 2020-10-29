eb93f09d07d4f82b23f090af66eaba62835014fd
Karalee Rodgers <10665343@uvu.edu>
2020-10-29 11:22:05 -0600

    added a bunch of code that filters stuff....

--- star-wars-api/main.js
+++ star-wars-api/main.js
@@ -7,7 +7,7 @@ const mainContent = document.querySelector('#main')
 
 const mainHeader = document.createElement('header')
 mainHeader.className = 'mainHeader'
-document.body.appendChild(mainHeader)
+document.body.insertBefore(mainHeader, mainContent)
 
 const planetButton = document.createElement('button')
 planetButton. textContent = 'Planets'
@@ -17,27 +17,72 @@ const speciesButton = document.createElement('button')
 speciesButton. textContent = 'Species'
 mainHeader.appendChild(speciesButton)
 
-const planetTypes = planets.filter(planets => planets.rotation_period === '24')
+const planetTypes = planets.filter((planets) => planets.rotation_period === '24')
 console.log(planetTypes)
 
-planetButton.addEventListener('click', event => {
+const speicesTypes = species.filter((species) => species.average_lifespan === '1000')
+
+planetButton.addEventListener('click', () => populateDOM(planetTypes))
+
+speciesButton.addEventListener('click', () => populateDOM(speicesTypes))
+
+/*planetButton.addEventListener('click', event => {
     planetTypes.forEach((element) => {
     const charFigure = document.createElement('figure')
     const charImg = document.createElement('img')
-    charImg.src = `https://starwars-visualguide.com/assets/img/planets/2.jpg`
+    let plaNum = getLastNumber(element.url)
+    charImg.src = `https://starwars-visualguide.com/assets/img/planets/${plaNum}.jpg`
+    charImg.addEventListener('error', event => {
+        console.log(event)
+        console.log(`Sorry, image not found for item #${plaNum}`)
+        charImg.hidden = true
+    })
     const charCaption = document.createElement('figcaption')
-    charCaption.textContent = 'Alderaan'
+    charCaption.textContent = element.name
 
     charFigure.appendChild(charImg)
     charFigure.appendChild(charCaption)
 
     mainContent.appendChild(charFigure)
 })
-})
+})*/
 
-function getLastNumber(url) {
-    console.log(url)
-
+function populateDOM(planets) {
+    removeChildren(mainContent)
+    planets.forEach((element) => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        let plaNum = getLastNumber(element.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/planets/${plaNum}.jpg`
+        charImg.addEventListener('error', event => {
+            console.log(event)
+            console.log(`Sorry, image not found for item #${plaNum}`)
+            charImg.hidden = true
+        })
+        const charCaption = document.createElement('figcaption')
+        charCaption.textContent = element.name
+    
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
+    
+        mainContent.appendChild(charFigure)
+  })
 }
 
-getLastNumber(url)
+//let theUrl = "https://swapi.co/api/planets/2/"
+//let theUrl2 = "https://swapi.co/api/planets/12/"
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+
+function removeChildren(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
