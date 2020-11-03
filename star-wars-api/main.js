ae1003490922a147b4af06463d4ba0392635d635
Karalee Rodgers <10665343@uvu.edu>
2020-11-03 11:27:17 -0700

    made new code for planets

--- star-wars-api/main.js
+++ star-wars-api/main.js
@@ -3,74 +3,36 @@ import { people } from './Data/people.js'
 import { planets } from './Data/planets.js'
 import { species } from './Data/species.js'
 
+const nav = document.querySelector('.nav')
+const navList = document.querySelector('.navList')
 const mainContent = document.querySelector('#main')
 
-const mainHeader = document.createElement('header')
-mainHeader.className = 'mainHeader'
-document.body.insertBefore(mainHeader, mainContent)
-
-const planetButton = document.createElement('button')
-planetButton. textContent = 'Planets'
-mainHeader.appendChild(planetButton)
-
-const speciesButton = document.createElement('button')
-speciesButton. textContent = 'Species'
-mainHeader.appendChild(speciesButton)
-
-const planetTypes = planets.filter((planets) => planets.rotation_period === '24')
-console.log(planetTypes)
-
-const speicesTypes = species.filter((species) => species.average_lifespan === '1000')
-
-planetButton.addEventListener('click', () => populateDOM(planetTypes))
-
-speciesButton.addEventListener('click', () => populateDOM(speicesTypes))
-
-/*planetButton.addEventListener('click', event => {
-    planetTypes.forEach((element) => {
-    const charFigure = document.createElement('figure')
-    const charImg = document.createElement('img')
-    let plaNum = getLastNumber(element.url)
-    charImg.src = `https://starwars-visualguide.com/assets/img/planets/${plaNum}.jpg`
-    charImg.addEventListener('error', event => {
-        console.log(event)
-        console.log(`Sorry, image not found for item #${plaNum}`)
-        charImg.hidden = true
-    })
-    const charCaption = document.createElement('figcaption')
-    charCaption.textContent = element.name
-
-    charFigure.appendChild(charImg)
-    charFigure.appendChild(charCaption)
-
-    mainContent.appendChild(charFigure)
-})
-})*/
-
-function populateDOM(planets) {
-    removeChildren(mainContent)
-    planets.forEach((element) => {
-        const charFigure = document.createElement('figure')
-        const charImg = document.createElement('img')
-        let plaNum = getLastNumber(element.url)
-        charImg.src = `https://starwars-visualguide.com/assets/img/planets/${plaNum}.jpg`
-        charImg.addEventListener('error', event => {
-            console.log(event)
-            console.log(`Sorry, image not found for item #${plaNum}`)
-            charImg.hidden = true
+function populateNav(planets) {
+    planets.forEach(planet => {
+        let anchorWrap = document.createElement('a')
+        anchorWrap.href = '#'
+        anchorWrap.addEventListener('click', event => {
+            let planetName = event.target.textContent
+            const foundPlanet = planets.find(planet => planet.name === planetName)
+            populatePlanetView(foundPlanet)
         })
-        const charCaption = document.createElement('figcaption')
-        charCaption.textContent = element.name
-    
-        charFigure.appendChild(charImg)
-        charFigure.appendChild(charCaption)
-    
-        mainContent.appendChild(charFigure)
-  })
+
+        let listItem = document.createElement('li')
+        listItem.textContent = planet.name
+
+        anchorWrap.appendChild(listItem)
+        navList.appendChild(anchorWrap)
+        nav.appendChild(navList)
+    })
 }
 
-//let theUrl = "https://swapi.co/api/planets/2/"
-//let theUrl2 = "https://swapi.co/api/planets/12/"
+function populatePlanetView(planetData) {
+    console.log(planetData)
+    const planetFig = document.createElement('figure')
+    const planetImg = document.createElement('img')
+    let planetNum = getLastNumber(element.url)
+    planetImg.src = `https://starwars-visualguide.com/assets/img/planets/${planetNum}.jpg`
+}
 
 function getLastNumber(url) {
     let end = url.lastIndexOf('/')
@@ -79,10 +41,5 @@ function getLastNumber(url) {
         start++
     }
     return url.slice(start, end)
-}
-
-function removeChildren(container) {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-}
+} 
+populateNav(planets)
